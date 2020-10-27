2040e1eb91b69ae52868194864d1911a1060b41f
apurvi96 <apurvi.mansinghka@students.iiit.ac.in>
2020-10-28 02:35:16 +0530

    page ranking algo

---
+++ scripts/page_rank.py
@@ -0,0 +1,66 @@
+import nltk
+import numpy as np
+import pandas as pd
+import re
+import glob
+from nltk.tokenize import sent_tokenize
+from nltk.corpus import stopwords
+
+word_embeddings = {}
+f = open('glove.6B.100d.txt', encoding='utf-8')
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    word_embeddings[word] = coefs
+f.close()
+
+stop_words = stopwords.words('english')
+
+
+
+
+# read tweets corresponding to the tag
+def get_sentences(tag):
+    files = glob.glob("data/tweets/"+tag+"/*")
+    sentences = []
+    for file in files:
+        fd = open(file,"r")
+        for line in fd:
+            sentences.append(line)
+    return sentences
+
+
+#tokenize received sentences
+def sentence_tokenize(tweets):
+    sentences = []
+    for tweet in tweets:
+        sentences.append(sent_tokenize(tweet))
+    sentences = [y for x in sentences for y in x]
+    return sentences
+
+
+def remove_stopwords(sen):
+    sen_new = " ".join([i for i in sen if i not in stop_words])
+    return sen_new
+
+
+# preprocessing --> lowercase, stopwords removal
+def text_processing(sentences):
+    clean_sentences = pd.Series(sentences).str.replace("[^a-zA-Z]", " ")
+    clean_sentences = [s.lower() for s in clean_sentences]
+    clean_sentences = [remove_stopwords(r.split()) for r in clean_sentences]
+    return clean_sentences
+
+
+# get vector representations using glove embedding
+def vector_representations(clean_sentences):
+    sentence_vectors = []
+    for i in clean_sentences:
+        if len(i) != 0:
+            v = sum([word_embeddings.get(w, np.zeros((100,))) for w in i.split()])/(len(i.split())+0.001)
+        else:
+            v = np.zeros((100,))
+        sentence_vectors.append(v)
+    return sentence_vectors
+
