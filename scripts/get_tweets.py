86e4954840d79ddb5667b42767e1b19c11390b76
jyotiGambhir <jyoti.gambhir@students.iiit.ac.in>
2020-10-28 10:57:14 +0530

    added summarization

--- scripts/get_tweets.py
+++ scripts/get_tweets.py
@@ -19,19 +19,12 @@ import pandas as pd
 import re
 import networkx as nx
 from nltk.corpus import stopwords
-from page_rank import get_sentences, sentence_tokenize,remove_stopwords,text_processing, vector_representations
+from page_rank import get_sentences, sentence_tokenize,remove_stopwords,text_processing, vector_representations, similarity_matrix,apply_pagerank,summary_extraction
+from scraper import get_autorization, fetch_and_clean_tweets
 
+MAX_TWEETS = 20000
+REQUIRED_TWEETS = 5000
 
-MAX_TWEETS = 200
-REQUIRED_TWEETS = 100
-
-CONSUMER_TOKEN = "jHTLoXi1itNEIboVkkG5PlZlM"
-CONSUMER_SECRET = "P4GsPqM4ms9amDLdHC69aKXk1gom7NH17apofBCDbMmp2uBgZ5"
-ACCESS_TOKEN = "554538683-luV4dNLwBP33ifxIjdCRMljx2wAftj01RcRvjhrI"
-ACCESS_TOKEN_SECRET = "kRoBfbr3HBnMKCcXVaPwGtIj8P2Mo2LmgXjw86JI2OOlf"
-auth = tweepy.OAuthHandler(CONSUMER_TOKEN, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
 
 tweets_folder = "data/tweets/"
 
@@ -50,55 +43,6 @@ def write_to_file(filename, tweet):
 	with open(filename, "w") as f: 
 		f.write(tweet) 
 
-def fetch_and_clean_tweets(hashtag,slang_dictionary):
-	if not os.path.isdir(tweets_folder+hashtag):
-		os.mkdir(tweets_folder+hashtag)
-	hash_tag_folder = tweets_folder+hashtag
-	count=0
-	seen = []
-	for tweet in tweepy.Cursor(api.search, q="#"+hashtag, lang="en", tweet_mode='extended').items(MAX_TWEETS):
-		if 'retweeted_status' in dir(tweet):
-			text=tweet.retweeted_status.full_text
-		else:
-			text=tweet.full_text
-		
-		if 'in_reply_to_status_id' in dir(tweet):
-			if tweet.in_reply_to_status_id != None:
-				continue
-
-		try:
-			if tweet.entities['media'][0]['type']=='photo':
-				continue
-
-			if tweet.entities['media'][0]['type']=='video':
-				continue
-		except:
-			pass
-
-		text = re.sub('&amp;','&',text)
-		text = re.sub(r'^RT[\s]+', '', text, flags=re.MULTILINE) # removes RT
-		text = re.sub(r'https?:\/\/.*[\r\n]*', '', text, flags=re.MULTILINE) #remove link
-		text = re.sub(r'[:]+', '', text, flags=re.MULTILINE)	
-		text = re.sub(r'[^\x00-\x7F]+','', text)
-		for wr in text:
-			if wr in slang_dictionary:
-				text= re.sub(r'\b'+wr+r'\b', slang_dictionary[wr],text)
-
-		new_text = ""		
-		for i in text.split(): # remove @ and #words, punctuataion
-			if not i.startswith('@') and not i.startswith('#') and i not in string.punctuation:
-				new_text+=i+" "	
-		text = new_text			
-		if len(text)>80:
-			if text in seen:
-				continue
-			else:
-				count+=1
-				seen.append(text)
-				write_to_file(tweets_folder+hashtag+"/"+str(count), text)
-		if count>=REQUIRED_TWEETS:
-			break
-
 if not os.path.isdir("data/tweets"):
 	os.mkdir("data/tweets")
 slang_dictionary ={}
@@ -106,22 +50,27 @@ csvfile= open('Slang_Dictionary.csv','r')
 reader = csv.reader(csvfile)
 for row in reader :
 	slang_dictionary[str(row[0])]= str(row[1])
-
+api = get_autorization()
 while(1):
 	print("Enter a hashtag without the hash: ")
 	input_hashtag = input()
-	fetch_and_clean_tweets(input_hashtag,slang_dictionary)
+	fetch_and_clean_tweets(api, tweets_folder, input_hashtag, slang_dictionary, MAX_TWEETS)
 	sentences = get_sentences(input_hashtag)
 	print("Received tweets..............")
-	print(sentences[:5])
+	# print(sentences[:5])
 	sentences = sentence_tokenize(sentences)
-	print("Tokenized sentences............")
-	print(sentences[:5])
+	# print("Tokenized sentences............")
+	# print(sentences[:5])
 	clean_sentences = text_processing(sentences)
-	print("After preprocessing...............")
-	print(clean_sentences[:5])
+	print("Preprocessing data...............")
+	# print(clean_sentences[:5])
 	sentence_vectors = vector_representations(clean_sentences)
-	print("Vectors Received.................")
-	print(sentence_vectors:5)
-	
+	print("Vectors Representations Calculated.................")
+	# print(sentence_vectors[:5])
+	sim_mat = similarity_matrix(sentence_vectors)
+	print("Similarity matrix calculated.................")
+	scores = apply_pagerank(sim_mat)
+	print("page rank scores..............")
+	print("SUMMARY:")
+	summary_extraction(scores,sentences,10)
 	
